eee050a45edbae8fd74802432f3ac9009723daea
Raj <rajkumer2001@gmail.com>
2022-07-06 21:41:36 +0600

    edit login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{ asset('assets/images/favicon-32x32.png') }}" type="image/png" />
+    {{-- <link rel="icon" href="http://raj.connectbangladeshint.org/assets/images/favicon-32x32.png" type="image/png" /> --}}
+
     <!-- Bootstrap CSS -->
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
     {{-- <link href="{{ asset('assets/css/bootstrap-extended.css') }}" rel="stylesheet" /> --}}
@@ -29,50 +31,56 @@
         <main class="authentication-content">
             <div class="container-fluid">
                 <div class="authentication-card col-md-5">
-                    <div class="card card-body p-4 p-sm-5">
-                        <h5 class="card-title">Sign In</h5>
+                    <div style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;" class="card card-body p-4 p-sm-5">
+
+                        <center>
+                            <img style="width: 80px" src="{{ asset('assets/images/logo-icon1.png') }}" alt="">
+
+                        </center>
+                        <h6 class="text-center mt-1"><b>MAQ PAPER USER LOGIN</b> </h6>
+
+
                         <form class="form-body" method="POST" action="{{ route('loginpost') }}">
                             @csrf
 
 
-                                <div class="login-separater text-center mb-4">
+                            <div class="login-separater text-center mb-4">
+
+                            </div>
+                            <div class="row g-3">
+                                <div class="col-12">
+                                    <div class="ms-auto position-relative">
+                                        <div class="position-absolute top-50 translate-middle-y search-icon px-3">
+                                            <img style="width: 35px;margin-left:-10px"
+                                                src="{{ asset('assets/images/usr.jpg') }}" alt="">
+                                        </div>
+                                        <input type="email" name="email" class="form-control radius-30 ps-5"
+                                            id="inputEmailAddress" placeholder="User / Email">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+
+                                    <div class="ms-auto position-relative">
+                                        <div class="position-absolute top-50 translate-middle-y search-icon px-3">
+                                            <img style="width: 25px;margin-left:-8px"
+                                                src="{{ asset('assets/images/pas.png') }}" alt="">
+                                        </div>
+                                        <input type="password" name="password" class="form-control radius-30 ps-5"
+                                            id="inputChoosePassword" placeholder="Password">
+                                    </div>
+                                </div>
+                                <div class="col-6">
 
                                 </div>
-                                <div class="row g-3">
-                                    <div class="col-12">
-                                        <label for="inputEmailAddress" class="form-label">Email Address</label>
-                                        <div class="ms-auto position-relative">
-                                            <div class="position-absolute top-50 translate-middle-y search-icon px-3">
-                                                <i class="bi bi-envelope-fill"></i>
-                                            </div>
-                                            <input type="email" name="email" class="form-control radius-30 ps-5"
-                                                id="inputEmailAddress" placeholder="Email Address">
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <label for="inputChoosePassword" class="form-label">Enter
-                                            Password</label>
-                                        <div class="ms-auto position-relative">
-                                            <div class="position-absolute top-50 translate-middle-y search-icon px-3">
-                                                <i class="bi bi-lock-fill"></i>
-                                            </div>
-                                            <input type="password" name="password" class="form-control radius-30 ps-5"
-                                                id="inputChoosePassword" placeholder="Enter Password">
-                                        </div>
-                                    </div>
-                                    <div class="col-6">
-
-                                    </div>
-                                    <div class="col-6 text-end">
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="d-grid">
-                                            <button type="submit" class="btn btn-primary radius-30">Sign
-                                                In</button>
-                                        </div>
-                                    </div>
-                                    <br><br>
+                                <div class="col-6 text-end">
                                 </div>
+                                <div class="col-12">
+
+                                    <center><button style="padding-left: 20px;padding-right: 20px" type="submit" class="btn btn-primary text-center"><b>LOGIN</b> </button></center>
+
+                                </div>
+                                <br><br>
+                            </div>
                         </form>
                     </div>
                 </div>
